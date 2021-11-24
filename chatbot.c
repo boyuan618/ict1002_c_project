2d7a68fbf5f9774023e1b7855e992a5836c3e049
P3_2103210_Boyuan Gu <guboyuan@gmail.com>
2021-11-24 04:59:17 +0000

    added main.c

---
+++ chatbot.c
@@ -0,0 +1,7 @@
+/*chatbot.c implements the behaviour of the chatbot. It is required to: 
+1. Recognise smalltalk and provide an appropriate response. 
+2. Otherwise, identify the intent and entity in each line of input from the user. 
+3. If the intent is an instruction, carry it out. 
+4. If the intent is a question, search the knowledge base (see below) for an answer. 
+5. If the knowledge base does not contain an answer, ask the user for an answer, and add 
+the new answer to the knowledge base. 
---
+++ main.c
@@ -0,0 +1,2 @@
+/* main.c  contains  the  main  loop  and  some  utility  functions.  It  prompts  the  user  to  type  in  a  question, gathers the response, and divides it into words. You should not need to modify this file.
+*/
